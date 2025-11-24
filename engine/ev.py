62c34e8259fb27e9679f3cf6ac0c89dda0e43260
DamonTGC <damons06@icloud.com>
2025-11-23 23:47:41 -0700

    Delete engine directory

--- engine/ev.py
+++
@@ -1,3 +0,0 @@
-def calculate_ev(prob, prop):
-    odds=-110
-    return round((prob*100)-100,2)
--- engine/tiers.py
+++
@@ -1,4 +0,0 @@
-def assign_tier(ev):
-    if ev>=15: return "DIME"
-    if ev>=5: return "NICKEL"
-    return "STANDARD"
